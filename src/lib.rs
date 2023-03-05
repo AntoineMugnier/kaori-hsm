fd81ebe8d835b54dae45dd1eafe43e6ca16d89f1
Antoine Mugnier <amugnier@europe.com>
2023-03-06 00:15:10 +0100

    First path of implementation that appears viable

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,34 @@
 struct NoneState;
 
+mod Sealed{
+  
+}
+
 pub trait State<T: StateMachine>{
-    fn init(&self, sm : &mut T);
+    fn init(sm : &mut T);
 
-    fn entry(&self, sm: &mut T);
+    fn entry(sm: &mut T);
 
-    fn exit(&self, sm : &mut T);
+    fn exit(sm : &mut T);
 
-    fn handle(&self, sm : &mut T, evt: T::Evt);
+    fn handle(sm : &mut T, evt: T::Evt);
+    
+    fn core_handle(sm : &mut T, evt: CoreEvt<T::Evt>){
 
+    }
+}
+//Todo delete pub
+pub enum CoreEvt<USER_EVT_T>{
+    Init,
+    Entry,
+    Exit,
+    User{user_evt : USER_EVT_T}
 }
 
 pub trait StateMachine{
     type Evt;
-    fn dispatch(&mut self, evt : Self::Evt){}
-    fn get_curr_state(&mut self) -> Box<dyn State<Self>>;
-    
+    fn dispatch(&mut self, evt : CoreEvt<Self::Evt>){}
+    //fn get_curr_state(&mut self) -> fn(&mut StateMachine, evt: Self::Evt);
 }
 
 #[cfg(test)]
