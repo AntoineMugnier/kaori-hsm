5aae9ac1a60232a25db3e0b841edba41ccd5b8dd
Antoine Mugnier <amugnier@europe.com>
2023-03-22 22:47:50 +0100

    init transition chain developped

--- src/lib.rs
+++ src/lib.rs
@@ -155,19 +155,27 @@ impl <UserStateMachine : ProtoStateMachine>StateMachine<UserStateMachine>{
 
     pub fn reach_init_target(&mut self, target_state_fn : StateFn<UserStateMachine>) -> StateFn<UserStateMachine>{
         
-        let mut final_target_state = target_state_fn;
-
-        while let Some(next_target_state) = self.dispatch_init_evt(target_state_fn){ 
-            self.dispatch_entry_evt(next_target_state);
-            final_target_state = next_target_state;
+        let mut current_target_state_fn = target_state_fn;
+        
+        while let Some(next_target_state) = self.dispatch_init_evt(current_target_state_fn){ 
+            current_target_state_fn = next_target_state;
+            self.dispatch_entry_evt(current_target_state_fn);
         }
         
-        final_target_state
+        current_target_state_fn
     }
 
     pub fn init(&mut self){
+
+        // Call user top initial pseudostate implementation
         let init_result = self.user_state_machine.init();
-        self.curr_state = Some(self.reach_init_target(init_result.0.unwrap_or_else(|| panic!("Topmost Init should return a state"))));
+        let topmost_init_target_state_fn = init_result.0.unwrap_or_else(|| panic!("Topmost Init should return a state"));
+
+        // Reach leaf state
+        self.dispatch_entry_evt(topmost_init_target_state_fn);
+        let last_init_state_fn = self.reach_init_target(topmost_init_target_state_fn);
+
+        self.curr_state = Some(last_init_state_fn);
     }   
 
     fn handle_ignored_evt(&mut self, parent_state_variant : ParentState<UserStateMachine>,evt : &CoreEvt::<<UserStateMachine as ProtoStateMachine>::Evt>){
