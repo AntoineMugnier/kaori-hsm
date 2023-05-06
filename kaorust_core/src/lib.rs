bd33435eb5ba3f02d191bdeb6276bea0e0ab577f
AntoineMugnier <amugnier@europe.com>
2023-05-06 18:12:49 +0200

    more doc in lib.rs

--- kaorust_core/src/lib.rs
+++ kaorust_core/src/lib.rs
@@ -1,18 +1,24 @@
-//! Kaorust is a Hierarchical State Machine (HSM) framework developed in Rust at destination of both embedded system and desktop computers.
-//! The framwork has the advantages of being free of dynamic allocation and of virtual table.
+//! Kaorust is a Hierarchical State Machine (HSM) framework developed in Rust at destination of
+//! embedded system. It can also be used safely on and desktop computers.  
+//!
+//!# Performance
+//! The framework is designed to be lightweight, holding just a few kilobytes in memory.
+//! It also has the advantages of being free of dynamic allocation and of virtual tables.
+//! 
 //! To build your own state machine, you first have to define the structure that will hold its
 //! data and then you will need to implement The following traits of the framework on it: the `ProtoStateMachine`
 //! trait and as many variant of the `State` trait as you want to define states. After that, you
 //! will assemble a complete `StateMachine` by sending your state machine as argument to the
-//! `StateMachine::from()` function.
+//! `StateMachine::from()` function.  
+//!
 //! The ProtoStateMachine trait requires you to implement the `ProtoStateMachine::Init()` method
-//! which will be called at the initialization of the machine and setting the first state.
+//! which will be called at the initialization of the machine and set the first state.
 //! The trait also requires you to define the `ProtoStateMachine::Evt` type, which will be the type of the enum
 //! variants that you state machine will be able to accept in the `StateMachine::dispatch()`
 //! method.  
 //! The State trait is generic and must be implemented for each of the states you want to define for
 //! your HSM. Each implementation of the `State` trait will require you to implement the following
-//! methods:
+//! methods:  
 //! - `State::init()` : the initial transition. Only for leaf states. Triggered when
 //! this state is the target of the transition. Returns a target substate to which we will enter
 //! next.
@@ -20,6 +26,7 @@
 //! - `State::exit()`: the exit statement. Called each time we exit this state
 //! - `State::handle()`: the exit statement. Called every time we dispatch an event, only if this
 //! state is the current state of the HSM.
+//!
 mod proto_state_machine;
 mod state;
 mod state_machine;
